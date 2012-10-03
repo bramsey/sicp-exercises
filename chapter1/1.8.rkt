d5d9e62d0ab61d35beb350191aec2cb5b196a12b
Bill Ramsey <bill@billiamram.com>
2012-10-03 17:35:22 -0400

    exercise 1.8

--- chapter1/1.8.rkt
+++ chapter1/1.8.rkt
@@ -1,3 +1,20 @@
 #lang racket
 (require racket/include)
 (include "../common.rkt")
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) 
+        (* 2 guess)) 
+     3))
+
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess)) 
+     (* guess .001)))
+
+(define (cubert-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cubert-iter (improve guess x) x)))
+
+(define (cubert x)
+  (cubert-iter 1.0 x))
