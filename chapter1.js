a26288896960b1ef3563f49d036fcbcddde6fee4
Bill Ramsey <bill@billiamram.com>
2012-10-03 13:50:13 -0400

    exercise 1.1

--- chapter1.js
+++ chapter1.js
@@ -1,2 +1,36 @@
 // non-exercise helpers
 function square(x) { return x*x; }
+
+// Exercises
+
+// 1.1
+10;
+//= 10
+5 + 3 + 4;
+//= 12
+9 - 1;
+//= 8
+6 / 2;
+//= 3
+(2 * 4) + (4 - 6);
+//= 6
+var a = 3;
+var b = a + 1;
+a + b + (a * b);
+//= 19
+a === b;
+//= false
+(b > a && b < (a * b)) ? b : a;
+//= 4
+if (a === 4) {
+  6;
+} else if (b === 4) {
+  6 + 7 + a;
+} else {
+  25;
+}
+//= 16
+2 + (b > a ? b : a);
+//= 6
+(a > b ? a : (a < b ? b : -1)) * (a + 1);
+//= 16
